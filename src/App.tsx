0328151b919cc5f83f6d697ee0b8dd5bdbc2c492
Yasmeen <yelzamek@udel.edu>
2023-02-10 13:17:45 -0500

    new hello world

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Yasmeen Hello World
+                Hello World! Edit <code>src/App.tsx</code> and save. This page
+                will automatically reload. Yasmeen Hello World
             </p>
         </div>
     );
