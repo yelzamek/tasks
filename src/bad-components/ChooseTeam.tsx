5bb61892dbe87f61865500aac35370f3de0d8098
Yasmeen <yelzamek@udel.edu>
2023-03-28 18:44:15 -0400

    wrapping up task 9- single test fails bc of header issue (???) but otherwise complete

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,21 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
+    function chooseMember(member: string) {
         /*
         if (!team.includes(newMember)) {
             team.push(newMember);
         }
         */
+        !team.includes(member) ? setTeam([...team, member]) : team;
     }
 
     function clearTeam() {
         /*
         team = [];
         */
+        setTeam([]);
+        setAllOptions([...PEOPLE]);
     }
 
     return (
@@ -36,7 +39,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
