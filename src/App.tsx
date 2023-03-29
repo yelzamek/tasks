f13516ea0d8341b3299f2318db7da53c1509546b
Yasmeen <yelzamek@udel.edu>
2023-03-28 22:56:55 -0400

    resolved merge conflict w App.tsx and setting up for task 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
-import yasmin_face from "./img/yasmin-face.jpg";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -12,52 +10,33 @@ import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { CheckAnswer } from "./form-components/CheckAnswer";
+import { GiveAttempts } from "./form-components/GiveAttempts";
+import { EditMode } from "./form-components/EditMode";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+import { ChangeColor } from "./form-components/ChangeColor";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
-                <h1 className="header-text">This is header text!</h1>
             </header>
-            <p>
-                Hello World! Edit <code>src/App.tsx</code> and save. This page
-                will automatically reload. Yasmeen Hello World
-            </p>
-            Unordered list:
-            <div className="list">
-                <ul>
-                    <li>eggs</li>
-                    <li>milk</li>
-                    <li>butter</li>
-                </ul>
-            </div>
-            <img className="face-img" src={yasmin_face} alt="my linkedin pfp" />
-            {/* Doesn't actually log hello world anywhere... */}
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            First column. This column is supposed to have some
-                            content to show that it stays in its own column on
-                            its own side of the screen.
-                            <div className="red-rect"></div>
-                        </Col>
-                        <Col>
-                            Second column. Anything can go in here. I could try
-                            adding an image in here just to see that it would
-                            stay on its own half of the screen also. We will
-                            see.
-                            <div className="red-rect"></div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
             <hr></hr>
-            <DoubleHalf></DoubleHalf>
+            <CheckAnswer expectedAnswer="42"></CheckAnswer>
+            <hr></hr>
+            <GiveAttempts></GiveAttempts>
+            <hr></hr>
+            <EditMode></EditMode>
+            <hr></hr>
+            <ChangeColor></ChangeColor>
+            <hr></hr>
+            <MultipleChoiceQuestion
+                options={["a", "b", "c"]}
+                expectedAnswer="b"
+            ></MultipleChoiceQuestion>
+            <hr></hr>
+            {/* <DoubleHalf></DoubleHalf> */}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
@@ -69,11 +48,11 @@ function App(): JSX.Element {
             <hr />
             <RevealAnswer></RevealAnswer>
             <hr />
-            <ChangeType></ChangeType>
-            <hr />
             <StartAttempt></StartAttempt>
             <hr />
             <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
         </div>
