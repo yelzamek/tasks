0182124364c6b322f04ae22b98d3e82f05796fa2
Yasmeen <yelzamek@udel.edu>
2023-03-28 23:55:03 -0400

    task 10 complete! all tests pass

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,55 @@
+import { cpuUsage } from "process";
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const COLORS = [
+        "red",
+        "blue",
+        "green",
+        "yellow",
+        "orange",
+        "white",
+        "black",
+        "magenta",
+        "cyan"
+    ];
+    const default_color = COLORS[0];
+    type ChangeEvent = React.ChangeEvent<
+        HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+    >;
+    const [color, setColor] = useState<string>(default_color);
+    function updateColor(event: ChangeEvent) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                {COLORS.map((currColor: string) => (
+                    <Form.Check
+                        inline
+                        key={currColor}
+                        name="colors"
+                        type="radio"
+                        label={currColor}
+                        value={currColor}
+                        id="color-options-radio-buttons"
+                        checked={currColor === color}
+                        onChange={updateColor}
+                        style={{ backgroundColor: currColor }}
+                    ></Form.Check>
+                ))}
+            </div>
+            <div>
+                You have chosen{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: color }}
+                >
+                    {color}.
+                </span>
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequired, setAttemptsRequired] = useState<string>("0");
+    const attemptsRequiredInt = parseInt(attemptsRequired) || 0;
     return (
         <div>
             <h3>Give Attempts</h3>
+            Attempts Left: {attemptsLeft}
+            <Form.Group>
+                <Form.Control
+                    type="number"
+                    value={attemptsRequired}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttemptsRequired(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <Button
+                onClick={() => setAttemptsLeft(attemptsLeft - 1)}
+                disabled={attemptsLeft == 0}
+            >
+                Use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttemptsLeft(attemptsLeft + attemptsRequiredInt)
+                }
+            >
+                Gain
+            </Button>
         </div>
     );
 }
